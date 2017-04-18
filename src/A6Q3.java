2da87853034f9d7c8166714dfb6e7d38e0bb89df
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-04-18 09:25:34 -0400

    School, April 13

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -19,23 +19,37 @@ public class A6Q3 {
         // Create input scanner
         Scanner input = new Scanner(System.in);
         
-        //create array
+        //create array for integers
         int[] num = new int[2];
         
         //Ask them to enter the numbers
         System.out.println("Please enter the two integers");
+        
         //create for loop to place characters in the array
         for (int i = 0; i < num.length; i++) {
             num[i] = input.nextInt();
         }
-        //if first number is greater then the second
-        if(num[0]>num[1]){
-            System.out.println("The Digits in accending order are : "+num[1]+", "+num[0]);
-        }
-        
-        //if the number is less than the second number
+        //Create integers to changwe the array
+        int a = num[1];
+        int b = num[0];
+         
+        //if the number is less than the second number the order of the array will stay the same
         if(num[0]<num[1]){
+            
+            //Print out the same array as before
             System.out.println("The Digits in accending order are : "+num[0]+", "+num[1]);
         }
+        
+        //if first number is greater then the second change the order for the numbers in the array 
+        if(num[0]>num[1]){
+            
+            //Change the values of places int he array to match asending order
+            num[0] = a;
+            num[1]=b;
+            
+            //Print out new array
+            System.out.println("The Digits in accending order are : "+num[0]+", "+num[1]);
+        }
+ 
     }
 }
