4cf94e3c9073aa0a147da98e018ae3aba4a7106d
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-04-21 09:43:25 -0400

    School, April 19

--- src/A6Q4.java
+++ src/A6Q4.java
@@ -18,34 +18,37 @@ public class A6Q4 {
         // Create an input scanner
         Scanner input = new Scanner(System.in);
 
-        //Create counter to change places in the array
-        int t = 0;
-
         System.out.println("Please enter the ten marks");
 
         //Create a array for the 10 marks
         double marks[] = new double[10];
-        for (int i = 0; i < marks.length; i++) {
+       
+        //Go thorugh all the places of the array
+        for (int i = 0; i < 10; i++) {
+            //Store the value inside the array
             marks[i] = input.nextDouble();
         }
         System.out.println("The numbers in acsending order are:");
         //Do the promgram while the counter is less then the length of the array so it covers all the places in the array
         for (int r = 0; r < 10; r++) {
-
-
-            for (int i = 0; i < 9; i++) {
-                if (marks[i] < marks[i + 1]) {
+            //Go throught the values of the second number being compared
+            for (int i = 1; i < 10; i++) {
+                //Change aroung the values if the first number is greater than the other one
+                if (marks[i] > marks[i - 1]) {
+                    
+                    //Create variables to represent the values og the first and second place being compared
                     double x = marks[i];
-                    //for (int j = 1; j < marks.length; j++) {
-                    double y = marks[i + 1];
+                    double y = marks[i - 1];
 
-
+                    //Change the variables for the position on the arrays around
                     marks[i] = y;
-                    marks[i + 1] = x;
-
-                    System.out.println(marks[i]);
+                    marks[i - 1] = x;
                 }
             }
+        }
+        //Do through the array in backwards order to show the numbers in ascendinig order
+        for (int i = 9; i> -1; i--) {
+            System.out.println(marks[i]);
         }
     }
 }
