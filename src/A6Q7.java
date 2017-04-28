6e5992cafeefb2402a4274d22c4ef02513cce55b
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-04-28 09:58:01 -0400

    School, April 28

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -19,14 +19,14 @@ public class A6Q7 {
         Scanner input = new Scanner(System.in);
 
         //Create counter for going through each spot in the array to find the prime numbers
-        int x = 1;
+        int x = 0;
 
         //Create an array for the cvonsecutive numbers
         int sieve[] = new int[1001];
 
         //Have the start of the array be two and the rest following two consecutivly
-        for (int i = 2; i < sieve.length; i++) {
-            sieve[i] = i;
+        for (int i = 1; i < sieve.length; i++) {
+            sieve[i] = i + 1;
         }
         //Create an array to store the prime numbers
         //int prime[] = new int[1001];
@@ -36,29 +36,45 @@ public class A6Q7 {
 
         System.out.println("Prime numbers:");
 
-        while (x < 1001) {
+        int prime[] = new int[1001];
+
+        while (x <= 1001) {
             //Go throught the seive values to check when the number in the sequence 
             //isn't the same as the prime multiple
-            for (int i = 2; i < sieve.length; i++) {
+            for (int i = 1; i < sieve.length; i++) {
                 //Go throught the values of the prime multiples
-                for (int j = 2; j < sieve.length; j++) {
-                    //Check if the number in sequence is a prime or if the number
-                    //isn't already a prime number used
-                    if (sieve[i] != (sieve[j] - 1) * P || i >= P) {
+                for (int j = 1; j < sieve.length; j++) {
+                //Check i++ the number in sequence is a prime or if the number
+                //isn't already a prime number used
+                //if(sieve[i]!=(sieve[j] - 1)*P){
+                //P=i;
+                // System.out.println("P is: "+i);
+                //}
+                
+                    if (sieve[i] != (sieve[j] - 1) * P) {
                         prime[i] = i;
-                      
-                        }
-                    }  if(x==1000){
-                            System.out.println(i);
+                        
+                    }
+                    }
+
+                
+                
+                P++;
+                x++;
+            }
+            if (x == 1001) {
+                for (int i = 1; i < sieve.length; i++) {
+                    System.out.println(prime[i]);
                 }
+
             }
             //Fix this!!!!!!!!!!!!!!!!!!!!!!!!
-            P++;
-           x++;
+
+
+
+
+
+
         }
-
-
-
-
     }
 }
