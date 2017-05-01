bd11794fbc9e0f9cd98d3e4ee477f6a4ff96a389
laveh2107 <laveh2107@HRH-IC0037387.wrdsb.ca>
2017-05-01 12:37:27 -0400

    MSIP May 1st

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * and open the template in the editor.
  */
 /**
+ * Find the prime numbers between 2 and 1000
  *
  * @author laveh2107
  */
@@ -18,63 +19,31 @@ public class A6Q7 {
         //Create input scanner
         Scanner input = new Scanner(System.in);
 
-        //Create counter for going through each spot in the array to find the prime numbers
-        int x = 0;
-
         //Create an array for the cvonsecutive numbers
         int sieve[] = new int[1001];
-
-        //Have the start of the array be two and the rest following two consecutivly
-        for (int i = 1; i < sieve.length; i++) {
-            sieve[i] = i + 1;
+        
+        //fill the array with numbers starting at 2 and add consecutivly
+        for (int i = 2; i < sieve.length; i++) {
+            sieve[i] = i;
         }
-        //Create an array to store the prime numbers
-        //int prime[] = new int[1001];
 
-        //Create a counter to keep track of the prime number
-        int P = 2;
-
-        System.out.println("Prime numbers:");
-
-        int prime[] = new int[1001];
-
-        while (x <= 1001) {
-            //Go throught the seive values to check when the number in the sequence 
-            //isn't the same as the prime multiple
-            for (int i = 1; i < sieve.length; i++) {
-                //Go throught the values of the prime multiples
-                for (int j = 1; j < sieve.length; j++) {
-                //Check i++ the number in sequence is a prime or if the number
-                //isn't already a prime number used
-                //if(sieve[i]!=(sieve[j] - 1)*P){
-                //P=i;
-                // System.out.println("P is: "+i);
-                //}
-                
-                    if (sieve[i] != (sieve[j] - 1) * P) {
-                        prime[i] = i;
-                        
-                    }
-                    }
-
-                
-                
-                P++;
-                x++;
-            }
-            if (x == 1001) {
-                for (int i = 1; i < sieve.length; i++) {
-                    System.out.println(prime[i]);
+        //Go throught the numbers to find the multiples of the prime  number
+        for (int i = 2; i < sieve.length; i++) {
+            //Get the multiples at the position "i"
+            for (int j = 0; j < 1001; j = j + i) {
+                if (j > i) {
+                    //set sieve position at 0 to get rid of later
+                    sieve[j] = 0;
                 }
-
             }
-            //Fix this!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
         }
+        //Print out the other numbers that arne't zero's
+        for (int i = 0; i < sieve.length; i++) {
+            //Get rid of the multiples of the position, that aren't zero's
+            if (sieve[i] != 0) {
+                System.out.println(sieve[i]);
+            }
+        }
+
     }
 }
